e66c9df88e783b8d94d4d929ef038ac8a0602dee
Sachin R <sirhalostreams@gmail.com>
2019-05-23 10:30:52 -0500

    update

---
+++ djangoproject1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\sachi\documents\github\djangopersonalprojects\djangoproject1\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
